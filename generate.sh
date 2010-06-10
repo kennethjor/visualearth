a9e0514b30baea31be314bafbe4c70dcd6b8ffac
Kenneth Jorgensen <kennethjor@gmail.com>
2010-06-10 19:45:27 +0100

    Added command line options to generate.sh; Added makevideo.sh script

--- generate.sh
+++ generate.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Default options
+SIZE=1920x1080
+
+# Command line options
+for i in $*
+do
+	case $i in
+	--save)
+		SAVE=true
+		;;
+	--size=*)
+		SIZE=`echo $i | sed 's/[-a-zA-Z0-9]*=//'`
+		;;
+	*)
+		echo "Unknown option $i" >&2
+		exit 1
+		;;
+	esac
+done
+
 mkdir render
 
 # Update clouds
@@ -19,5 +39,10 @@ rm config.tmp
 
 # Crop and resize
 # You may want to change the resize to fit your purposes
-convert -crop 4800x1940+0+230 -resize 1920x1080\! render.jpg render/earth.jpg
+convert -crop 4800x1940+0+230 -resize $SIZE\! render.jpg render/earth.jpg
 rm render.jpg
+
+# Save generated image
+if [ $SAVE ]
+then cp render/earth.jpg render/earth_`date -u +%F_%T`.jpg
+fi
